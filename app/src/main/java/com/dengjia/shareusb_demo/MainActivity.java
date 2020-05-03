1caf5c87f5efaa3f8b8223036cc223273dcc7724
JiaDeng <280858922@qq.com>
2020-05-03 19:47:45 +0800

    全屏;隐藏任务栏

--- app/src/main/java/com/dengjia/shareusb_demo/MainActivity.java
+++ app/src/main/java/com/dengjia/shareusb_demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.dengjia.shareusb_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
     private Button bt_clear_received_data;
     private Button bt_clear_send_data;
     private Button bt_send_data;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
